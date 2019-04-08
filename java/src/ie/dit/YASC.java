a81def116b05129907e7e3e5ee625e12e52417a8
PhilToolan <c17433026@mydit.ie>
2019-04-08 11:29:10 +0100

    week11

--- java/src/ie/dit/YASC.java
+++ java/src/ie/dit/YASC.java
@@ -1,65 +1,3 @@
-<<<<<<< HEAD
-package ie.dit;
-
-import java.util.ArrayList;
-
-import processing.core.PApplet;
-
-public class YASC extends PApplet
-{
-    boolean[] keys = new boolean[1024];
-
-    public ArrayList<GameObject> gameObjects = new ArrayList<GameObject>(); 
-
-    AIShip aiShip;
-
-    public void keyPressed()
-    {
-        keys[keyCode] = true;
-    }
-    public void keyReleased()
-    {
-        keys[keyCode] = false;
-    }
-
-    public boolean checkKey(int c)
-    {
-        return keys[c] || keys [Character.toUpperCase(c)];
-    }
-    
-    public void settings()
-    {
-        size(500, 500);        
-    }
-
-    public void setup()
-    {
-        gameObjects.add(new Ship(this, width / 2, height / 2, 5, 50));
-        aiShip = new AIShip(this, 100, 100, 5, 50);
-        gameObjects.add(aiShip);
-    }
-
-    public float timeDelta;
-    private float last;
-    public void draw()
-    {
-        float now = millis();
-        timeDelta = (now - last) / 1000.0f;
-        last = now;
-        background(255);
-        
-        fill(0);
-        text("GameObjects: " + gameObjects.size(), 50, 100);
-        for(int i= gameObjects.size() - 1; i >= 0; i--)
-        {
-            GameObject b = gameObjects.get(i);
-            b.render();
-            b.update();
-        }        
-    }
-
-
-=======
 package ie.dit;
 
 import java.util.ArrayList;
@@ -134,5 +72,4 @@ public class YASC extends PApplet
     }
 
 
->>>>>>> 4f1faacfbd5bb5fcbf46c4391114f3758db13162
 }
